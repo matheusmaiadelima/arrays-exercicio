94f45d63761e8cc911b8f09674bb012740b108d6
114532976 <matutyahu@gmail.com>
2022-10-27 16:55:15 -0300

    Exercícios Arrays

---
+++ exercicio1/script.js
@@ -0,0 +1,16 @@
+/* Exercício 1
+No primeiro exercício vamos praticar a criação de arrays. Crie 3 arrays diferentes:
+
+O primeiro deve possuir apenas números, e estes números não devem seguir nenhuma ordem específica;
+
+O segundo array deve possuir apenas strings;
+
+O terceiro array deve possuir números, strings e booleanos. Imprima cada um dos arrays acima, utilizando console.log() */
+
+const arrayNumber = [2, 8, 21]
+const arrayString = ["Matheus", "Raffael", "Vitor"]
+const arrayVarios = ["William", 18, true]
+
+console.log(arrayNumber)
+console.log(arrayString)
+console.log(arrayVarios)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,27 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+/* Exercício 2
+Com os arrays criados, agora vamos observar os arrays. Faça o que se pede abaixo, utilizando console.log():
+
+Imprima a quantidade de itens de cada array (utilize um console.log() para cada impressão);
+
+Imprima o primeiro item do primeiro array, o segundo item do segundo array, e o terceiro item do terceiro array;
+
+Verifique se um item está incluído no primeiro array e depois no terceiro array e imprima o resultado destas verificações no console.log(). A primeira impressão deve retornar um booleano true e a segunda deve retornar um booleano false. */
+
+const arrayNumber = [2, 8, 21]
+const arrayString = ["Matheus", "Raffael"]
+const arrayVarios = ["William", "Vitor", 14, 18, true]
+
+console.log("O número de itens no primeiro array é:", arrayNumber.length)
+console.log("O número de itens no segundo array é:", arrayString.length)
+console.log("O número de itens no terceiro array é:", arrayVarios.length)
+
+console.log("**************************************************")
+
+console.log("O primeiro item do primeiro array é:", arrayNumber[0])
+console.log("O segundo item do segundo array é:", arrayString[1])
+console.log("O terceiro item do terceiro array é:", arrayVarios[2])
+
+console.log("**************************************************")
+
+console.log(arrayNumber.includes(2))
+console.log(arrayVarios.includes(2))
